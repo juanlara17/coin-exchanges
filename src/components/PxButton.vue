dd8063f84a23c493d0345156c76b57ddafdaa662
juanlara17 <juanlara17@hotmail.com>
2020-05-18 15:47:34 -0500

    Production

---
+++ src/components/PxButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    @click="buttonClick"
+    class="w-100 bg-transparent hover:bg-green-500 focus:outline-none text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:boder-transparent rounded"
+  >
+    <beat-loader
+      class="w-100"
+      :loading="isLoading"
+      :color="'#68d391'"
+      :size="8"
+    />
+    <p v-show="!isLoading">
+      <slot></slot>
+    </p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "PxButton",
+
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    buttonClick() {
+      this.$emit("custom-click");
+    }
+  }
+};
+</script>
---
+++ src/views/Error.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex-col items-center">
+    <div class="text-gray-700 text-6xl uppercase">Error</div>
+    <router-link to="/" class="mt-5 text-lg text-green-600 hover:underline"
+      >Volver la pagina de Inicio</router-link
+    >
+  </div>
+</template>
